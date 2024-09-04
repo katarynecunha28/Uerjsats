7784eb44b009db32ccd3f0599471326214f5d6ce
katarynecunha28 <79368588+katarynecunha28@users.noreply.github.com>
2024-09-04 19:06:07 -0300

    Add files via upload

---
+++ Missão Kurumim/Suprimento_energia_.ino
@@ -0,0 +1,101 @@
+#include <Wire.h>
+#include <DallasTemperature.h>
+#include <Adafruit_INA219.h>
+
+// Definir pinos dos sensores DS18B20
+#define DS18B20_1_PIN 2
+#define DS18B20_2_PIN 3
+#define DS18B20_3_PIN 4
+
+// Inicializa o OneWire e DallasTemperature para cada sensor em pinos diferentes
+OneWire oneWire1(DS18B20_1_PIN);
+DallasTemperature sensor1(&oneWire1);
+
+OneWire oneWire2(DS18B20_2_PIN);
+DallasTemperature sensor2(&oneWire2);
+
+OneWire oneWire3(DS18B20_3_PIN);
+DallasTemperature sensor3(&oneWire3);
+
+Adafruit_INA219 ina219;
+
+unsigned long tempoInicial = 0;
+bool comandoRecebido = false;
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Inicializa os sensores DS18B20
+  sensor1.begin();
+  sensor2.begin();
+  sensor3.begin();
+
+  // Inicializa o INA219
+  if (!ina219.begin()) {
+    Serial.println("Falha ao encontrar INA219!");
+    while (1) { delay(10); }
+  } 
+
+  // Calibra o INA219 para leituras mais precisas (padrão para 32 V, 2A)
+  ina219.setCalibration_32V_2A();
+}
+
+void loop() {
+  receberComandoDoMaster(2); // Recebe os comandos destinados ao Suprimento de Energia
+  delay(1000);
+  
+  // Ler a tensão do barramento em volts
+  float busVoltage = ina219.getBusVoltage_V();
+  // Ler a corrente em miliamperes
+  float current_mA = ina219.getCurrent_mA();
+    
+  // Printa os valores da tensão do barramento e corrente na serial
+  Serial.print("Tensão do Barramento:   "); Serial.print(busVoltage); Serial.println(" V");
+  Serial.print("Corrente:       "); Serial.print(current_mA); Serial.println(" mA");
+  Serial.println();
+
+  // Lê a Temperatura dos sensores
+  sensor1.requestTemperatures();
+  sensor2.requestTemperatures();
+  sensor3.requestTemperatures();
+
+  float tempbateria1 = sensor1.getTempCByIndex(0);
+  float tempbateria2 = sensor2.getTempCByIndex(0);
+  float tempbateria3 = sensor3.getTempCByIndex(0);
+
+  // Printa os valores de temperatura das baterias
+  Serial.print("Temperatura da Bateria 1: "); Serial.println(tempbateria1);
+  Serial.print("Temperatura da Bateria 2: "); Serial.println(tempbateria2);
+  Serial.print("Temperatura da Bateria 3: "); Serial.println(tempbateria3);
+  Serial.println();
+
+  Serial.println("Comando recebido: " + String(comandoRecebido));
+
+  if (comandoRecebido) {
+    enviarDadosParaMaster("Dados do Suprimento de Energia: " + String(busVoltage) + "V " + String(current_mA) + "mA " + String(tempbateria1) + "C " + String(tempbateria2) + "C " + String(tempbateria3) + "C", 4); // Envia dados para o Master
+    comandoRecebido = false; // Reinicia a flag de comando recebido
+  }
+
+  delay(1000); // Aguarda 1 segundo antes de ler novamente
+}
+
+bool receberComandoDoMaster(int endereco) {
+  if (Serial.available() > 0) {  //Lê a serial e vê se tem algum comando
+    String dadosRecebidos = Serial.readStringUntil('\n');
+    int enderecoRecebido = dadosRecebidos.substring(0, 1).toInt();
+    
+    if (enderecoRecebido == endereco) { //Se o endereço recebido for igual a 2, ele printa os dados recebidos do master
+      String dados = dadosRecebidos.substring(2);
+      Serial.println("Comando recebido do Master: " + dados);
+      comandoRecebido = true; //flag que define que um comando foi recebido
+      tempoInicial = millis(); // Salva o tempo inicial para enviar a resposta após 100ms
+    }
+  }
+  return comandoRecebido;
+}
+
+void enviarDadosParaMaster(String dados, int endereco) {
+  Serial.print(endereco);  // Printar no monitor serial
+  Serial.print(": ");
+  Serial.println(dados);
+}
