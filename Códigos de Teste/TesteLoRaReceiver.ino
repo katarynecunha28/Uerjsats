2fab41c6704480de46281dc0693386fea515ca77
DanielKampffe <141583331+DanielKampffe@users.noreply.github.com>
2024-09-28 11:28:46 -0300

    Teste para ESP32 LoRa Heltec

---
+++ Códigos de Teste/TesteLoRaReceiver.ino
@@ -0,0 +1,60 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+// Pinos definidos especificamente para o ESP32 Heltec com LoRa
+#define LORA_SS 18
+#define LORA_RST 14
+#define LORA_DIO0 26
+
+// Endereço que o receptor deve aceitar
+#define RECEIVER_ADDRESS 42
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  // Configura os pinos do LoRa
+  LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
+
+  // Inicializa o módulo LoRa na frequência desejada
+  if (!LoRa.begin(915E6)) { // Altere a frequência conforme necessário
+    Serial.println("Erro ao iniciar o módulo LoRa!");
+    while (1);
+  }
+
+  Serial.println("Módulo LoRa iniciado com sucesso!");
+}
+
+void loop() {
+  // Verifica se há algum pacote recebido
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+
+    // Lê o endereço de destino do pacote
+    int destinationAddress = LoRa.read();
+
+    // Verifica se o endereço de destino é o correto
+    if (destinationAddress == RECEIVER_ADDRESS) {
+      // Lê a mensagem
+      String receivedMessage = "";
+      while (LoRa.available()) {
+        receivedMessage += (char)LoRa.read();
+      }
+
+      // Imprime a mensagem recebida no Serial Monitor
+      Serial.print("Mensagem recebida do endereço ");
+      Serial.print(destinationAddress);
+      Serial.print(": ");
+      Serial.println(receivedMessage);
+
+      //print RSSI of packet
+      int rssi = LoRa.packetRssi();
+      Serial.print(" with RSSI ");    
+      Serial.println(rssi);
+
+    } else {
+      // Ignorar pacotes com endereços de destino diferentes
+      Serial.println("Pacote ignorado devido ao endereço de destino incorreto");
+    }
+  }
+}
